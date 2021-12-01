64b82df2f0de87572d88f679e3779c656ab6dc0d
Gary Holmes <gaz2050@msn.com>
2021-12-01 14:47:05 +0000

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from "./logo.svg";
+import React, { useReducer } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import {
+  Home,
+  Calculator,
+  Stopwatch,
+  PaceConverter,
+  RockPaperScissors,
+  LogonDB,
+  Whoops404,
+} from "./Pages";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/stopwatch" element={<Stopwatch />} />
+        <Route path="/paceconverter" element={<PaceConverter />} />
+        <Route path="/rockpaperscissors" element={<RockPaperScissors />} />
+        <Route path="/logon" element={<LogonDB />} />
+        <Route path="/*" element={<Whoops404 />} />
+      </Routes>
     </div>
   );
 }
